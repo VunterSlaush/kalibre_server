a2be49e7173905bfd8682b383a7561006b6472ba
Jesus Mota <j3sus1506@gmail.com>
2018-01-28 21:05:44 -0400

    fix docs

--- docs/options.js
+++ docs/options.js
@@ -1,22 +1,22 @@
 var options = (env = "development") => {
-	return {
-		swaggerDefinition: {
-			info: {
-				title: "JobChat API",
-				description: "Endpoints provided by JobChat's API.",
-				version: "1.0"
-			},
-			schemes: env == "production" ? ["https"] : ["http", "https"],
-			basePath: "/api",
-			produces: ["application/json"],
-			security: [
-				{
-					basicAuth: []
-				}
-			]
-		},
-		apis: ["./docs/*.yaml", "./routes/*.js", "./models/*.js"]
-	};
+  return {
+    swaggerDefinition: {
+      info: {
+        title: "Klibre API",
+        description: "Endpoints provided by KLibre API.",
+        version: "1.0"
+      },
+      schemes: env == "production" ? ["https"] : ["http", "https"],
+      basePath: "/api",
+      produces: ["application/json"],
+      security: [
+        {
+          basicAuth: []
+        }
+      ]
+    },
+    apis: ["./docs/*.yaml", "./routes/*.js", "./models/*.js"]
+  };
 };
 
 module.exports = options;
